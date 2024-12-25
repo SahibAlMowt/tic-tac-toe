877244c664e307bdbeeb925cfb9d1823eaf5e8ea
SahibAlMowt <sahibshukurlu2006@gmail.com>
2024-12-25 21:03:19 +0400

    21:03

--- test1.cpp
+++ test1.cpp
@@ -14,5 +14,29 @@ TEST_SUITE("sam's tests")
 
         CHECK(examp.win('X'));
     }
+
+    TEST_CASE("bool draw")
+    {
+        game::Tictactoe examp(3 , "first", "second");
+
+        examp.move(0, 0, 'X');
+        examp.move(0, 1, 'O');
+        examp.move(0, 2, 'O');
+        examp.move(1, 0, 'O');
+        examp.move(1, 1, 'X');
+        examp.move(1, 2, 'X');
+        examp.move(2, 0, 'X');
+        examp.move(2, 1, 'O');
+        examp.move(2, 2, 'O');
+
+        CHECK(examp.draw());
+    }
+
+    TEST_CASE("bool move")
+    {
+        game::Tictactoe examp(3 , "first", "second");
+
+        CHECK(examp.move(0, 0, 'X'));
+    }
     
 }
